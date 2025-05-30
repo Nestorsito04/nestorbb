36962a7c491e67316cc5ba171cbffc2caedffe1c
Nestorsito04 <nestorrafaelbot@gmail.com>
2025-05-29 21:09:46 -0400

    Add files via upload

---
+++ 22222.cpp
@@ -0,0 +1,9 @@
+#include <iostream>  // Para operaciones de entrada/salida (cout, cin, endl)
+#include <fstream>   // Para manejar archivos (lectura y escritura)
+#include <string>    // Para trabajar con cadenas de texto (string)
+#include <algorithm> // Para usar la función std::transform (cambiar a minúsculas)
+#include <cctype>    // Para usar la función std::tolower (convertir caracteres a minúsculas)
+#include <windows.h> // Específica para Windows: control de la consola (colores, UTF-8)
+
+// Usamos el namespace std completo para simplificar el código
+using namespace std;
